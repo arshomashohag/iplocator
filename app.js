b3bc8c44091aaf9c25eac356a8223e42cd043627
shohag.mia <shohag.mia@bjitgroup.com>
2019-04-26 17:56:36 +0600

    Default page

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 
 const express = require('express');
+const router = express.Router();
 
 let app = express();
 
 app.listen(process.env.PORT || 3000);
 
 const getIpRouter = require('./router/get_ip');
+
+app.use('/', router.get('', function(req, res){
+    res.send('Welcome!');
+})
+);
 app.use('/get-ip', getIpRouter);
 
 console.log("Application running successfully.");
